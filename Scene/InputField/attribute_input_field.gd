b491065a73ca84cc1a488aa79712886b14886316
Spring <roman.spring@sithis.ch>
2024-06-12 21:11:58 +0200

    refactor: Refactor attribute input field

--- Scene/InputField/attribute_input_field.gd
+++ Scene/InputField/attribute_input_field.gd
@@ -1,7 +1,7 @@
 class_name AttributeInputField extends Control
 
 @onready var label: Label = %Label
-@onready var enum_input_field: OptionButton = %OptionButton
+@onready var list_input_field: OptionButton = %OptionButton
 @onready var string_input_field: ClipboardLineEdit = %ClipboardLineEdit
 @onready var number_input_field: ClipboardSpinBox = %ClipboardSpinBox
 
@@ -29,12 +29,10 @@ var editable: bool:
 		return editable
 	set(value):
 		editable = value
-		_set_editable(number_input_field, editable)
-		_set_editable(string_input_field, editable)
-		_set_editable(enum_input_field, editable)
+		_set_editable(editable)
 
 func _ready() -> void:
-	enum_input_field.hide()
+	list_input_field.hide()
 	string_input_field.hide()
 	number_input_field.hide()
 	label.custom_minimum_size = Vector2(label_width,0)
@@ -43,6 +41,7 @@ func init(attribute_name_: String) -> void:
 	attribute_name = attribute_name_
 	_set_value_to_field(Globals.language_string.GetAttribute(attribute_name).Value)
 	_init_fields()
+	_queue_free_all_hidden_fields()
 	label.text = _attribute.DisplayName + ":"
 
 func _init_fields():
@@ -58,10 +57,18 @@ func _init_fields():
 		string_input_field.show()
 		string_input_field.text_changed.connect(func(x): _on_field_value_changed(x))
 	elif _is_type_of_list():
-		enum_input_field.show()
+		list_input_field.show()
 		for enum_value in _attribute.EnumValues:
-			enum_input_field.add_item(enum_value)
-		enum_input_field.item_selected.connect(func(index):  _on_field_value_changed(enum_input_field.get_item_text(index)))
+			list_input_field.add_item(enum_value)
+		list_input_field.item_selected.connect(func(index):  _on_field_value_changed(list_input_field.get_item_text(index)))
+
+func _queue_free_all_hidden_fields():
+	if !number_input_field.visible:
+		number_input_field.queue_free()
+	if !string_input_field.visible:
+		string_input_field.queue_free()
+	if !list_input_field.visible:
+		list_input_field.queue_free()
 
 func _on_field_value_changed(new_value: String):
 	Globals.language_string.SetAttributeValue(_attribute.Name, new_value)
@@ -74,8 +81,8 @@ func _get_value_from_field() -> String:
 	elif _is_type_of_string():
 		return string_input_field.text
 	elif _is_type_of_list():
-		var index = enum_input_field.selected
-		return enum_input_field.get_item_text(index)
+		var index = list_input_field.selected
+		return list_input_field.get_item_text(index)
 	return ""
 
 func _set_value_to_field(value_: String):
@@ -89,10 +96,10 @@ func _set_value_to_field(value_: String):
 		_select_list_input_by_value(value_)
 
 func _select_list_input_by_value(value_: String):
-	for index in range(enum_input_field.item_count):
-		var value_at_index = enum_input_field.get_item_text(index)
+	for index in range(list_input_field.item_count):
+		var value_at_index = list_input_field.get_item_text(index)
 		if value_at_index == value_:
-			enum_input_field.select(index)
+			list_input_field.select(index)
 			return
 
 func _set_valid(is_valid: bool):
@@ -116,11 +123,15 @@ func _get_valid():
 		pass
 	return true
 
-func _set_editable(node: Node, editable_: bool):
-	if node is LineEdit or node is ClipboardLineEdit or node is ClipboardSpinBox:
-		node.editable = editable_
-	elif node is Button:
-		node.disabled = !editable_
+func _set_editable(editable_: bool):
+	if _is_type_of_int():
+		number_input_field.editable = editable_
+	elif _is_type_of_float():
+		number_input_field.editable = editable_
+	elif _is_type_of_string():
+		string_input_field.editable = editable_
+	elif _is_type_of_list():
+		list_input_field.disabled = !editable_
 
 func _is_type_of_int() -> bool:
 	return _attribute.IsTypeOf("Int")
