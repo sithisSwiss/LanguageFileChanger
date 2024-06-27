2ee6438ba737cc85e2a262a1292446c09e305849
Roman Spring <roman.spring@oetiker.com>
2024-06-27 12:59:48 +0200

    feat: Add apply to all button

--- Scene/Dialog/values_grid_container.gd
+++ Scene/Dialog/values_grid_container.gd
@@ -2,7 +2,21 @@ class_name ValuesGridContainer extends GridContainer
 
 @onready var clipboard_line_edit_scene := preload("res://Scene/InputField/clipboard_line_edit.tscn")
 
+const _value_group := "value_group"
+var apply_to_all_edit_line : ClipboardLineEdit
+
 func init():
+	var apply_to_all_btn = Button.new()
+	add_child(apply_to_all_btn)
+	apply_to_all_btn.text = tr("BUTTON_APPLY_TO_ALL")
+	apply_to_all_btn.size_flags_horizontal = SIZE_FILL
+	apply_to_all_btn.pressed.connect(_apply_to_all)
+	apply_to_all_btn.focus_mode = Control.FOCUS_NONE
+	
+	apply_to_all_edit_line = clipboard_line_edit_scene.instantiate() as ClipboardLineEdit
+	add_child(apply_to_all_edit_line)
+	apply_to_all_edit_line.size_flags_horizontal = SIZE_EXPAND_FILL
+	
 	for file in LanguageFileHelper.GetCurrentLanguageFilePaths():
 		_add_value_field(file)
 
@@ -14,6 +28,7 @@ func _add_value_field(file: String):
 
 	var edit_node := clipboard_line_edit_scene.instantiate() as ClipboardLineEdit
 	add_child(edit_node)
+	edit_node.add_to_group(_value_group)
 	edit_node.value = Globals.language_string.GetValueFromFile(file)
 	edit_node.size_flags_horizontal = SIZE_EXPAND_FILL
 	edit_node.value_changed.connect(func(new_text:String): Globals.language_string.SetValueToFile(file, new_text))
@@ -21,3 +36,11 @@ func _add_value_field(file: String):
 func clear():
 	for child in get_children():
 		remove_child(child)
+		
+func _apply_to_all():
+	var value = apply_to_all_edit_line.value
+	for child in get_children():
+		if child.is_in_group(_value_group):
+			var typed_child := child as ClipboardLineEdit
+			typed_child.value = value
+			typed_child._on_line_edit_text_changed(value)
